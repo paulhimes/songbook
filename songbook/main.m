7da9add39467270df6c59dca4365b1321cf96843
Paul Himes <paul@paulhimes.com>
2013-07-24 20:55:02 -0500

    Initial Commit

---
+++ songbook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  songbook
+//
+//  Created by Paul Himes on 7/24/13.
+//  Copyright (c) 2013 Paul Himes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ songbookTests/songbookTests.m
@@ -0,0 +1,36 @@
+//
+//  songbookTests.m
+//  songbookTests
+//
+//  Created by Paul Himes on 7/24/13.
+//  Copyright (c) 2013 Paul Himes. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface songbookTests : XCTestCase
+
+@end
+
+@implementation songbookTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
